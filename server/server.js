7c61fb9e90496acb2f17fcd0c77222cbc6e6bce2
Kanayo <anayoifediora@live.com>
2025-04-06 12:08:02 +0930

    modified server.js to ensure all routes are served

--- server/server.js
+++ server/server.js
@@ -43,6 +43,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get ('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const startApolloServer = async () => {
     //Start Apollo Server
     await server.start(); 
